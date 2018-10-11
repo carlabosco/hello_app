87fb3515c9a6c9e30b141a775ecb779aa1ecd885
Carla Bosco <carlabosco@gmail.com>
2018-10-11 12:06:26 -0700

    Update message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   def hello
-    render html: "¡ola, mundo!"
+    render html: "¡ola, mundo! Happy."
   end
 
   def goodbye
